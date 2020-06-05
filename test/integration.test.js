8b74a906e073777ebf7e7692a88df42abd7f0afe
Mathew Heard <splitice@users.noreply.github.com>
2020-06-05 13:43:38 +1000

    test fixes

--- test/integration.test.js
+++ test/integration.test.js
@@ -6,13 +6,13 @@ const pg = require("pg")
 const debug = require('debug')("pg-listen:test")
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-it("can connect", async t => {
+it("can connect", async () => {
   const hub = createPostgresSubscriber({ connectionString: "postgres://postgres:postgres@localhost:5432/postgres" })
   await hub.connect()
   await hub.close()
 })
 
-it("can listen and notify", async t => {
+it("can listen and notify", async () => {
   let connectedEvents = 0
   const notifications = []
   const receivedPayloads = []
@@ -43,13 +43,13 @@ it("can listen and notify", async t => {
     expect(receivedPayloads).to.be.eql([
       { hello: "world" }
     ])
-    t.is(connectedEvents, 1)
+    expect(connectedEvents).to.be.eql(1)
   } finally {
     await hub.close()
   }
 })
 
-it("can handle notification without payload", async t => {
+it("can handle notification without payload", async () => {
   const notifications = []
   const receivedPayloads = []
 
@@ -65,21 +65,21 @@ it("can handle notification without payload", async t => {
     await hub.notify("test")
     await delay(200)
 
-    t.deepEqual(hub.getSubscribedChannels(), ["test"])
-    t.deepEqual(notifications, [
+    expect(hub.getSubscribedChannels()).to.be.eql(["test"])
+    expect(notifications).to.be.eql([
       {
         channel: "test",
         payload: undefined,
         processId: notifications[0].processId
       }
     ])
-    t.deepEqual(receivedPayloads, [undefined])
+    expect(receivedPayloads).to.be.eql([undefined])
   } finally {
     await hub.close()
   }
 })
 
-it("can use custom `parse` function", async t => {
+it("can use custom `parse` function", async () => {
   const notifications = []
 
   const connectionString = "postgres://postgres:postgres@localhost:5432/postgres"
@@ -100,7 +100,7 @@ it("can use custom `parse` function", async t => {
     await client.query(`NOTIFY test, '${Buffer.from("I am a payload.", "utf8").toString("base64")}'`)
     await delay(200)
 
-    t.deepEqual(notifications, [
+    expect(notifications).to.be.eql([
       {
         channel: "test",
         payload: "I am a payload.",
@@ -112,7 +112,8 @@ it("can use custom `parse` function", async t => {
   }
 })
 
-it("getting notified after connection is terminated", async t => {
+it("getting notified after connection is terminated", async () => {
+  this.timeout(4000)
   let connectedEvents = 0
   let reconnects = 0
 
@@ -122,6 +123,7 @@ it("getting notified after connection is terminated", async t => {
   const connectionString = "postgres://postgres:postgres@localhost:5432/postgres"
   let client = new pg.Client({ connectionString })
   await client.connect()
+  client.on('error',()=>{})
 
   const hub = createPostgresSubscriber(
     { connectionString: connectionString + "?ApplicationName=pg-listen-termination-test" },
@@ -142,7 +144,7 @@ it("getting notified after connection is terminated", async t => {
     debug("Terminating database backend")
 
     // Don't await as we kill some other connection, so the promise won't resolve (I think)
-    client.query("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND usename = current_user")
+    client.query("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND usename = current_user").catch(()=>{})
     await delay(2000)
 
     client = new pg.Client({ connectionString })
@@ -152,19 +154,19 @@ it("getting notified after connection is terminated", async t => {
     await client.query(`NOTIFY test, '{"hello": "world"}';`)
     await delay(500)
 
-    t.deepEqual(hub.getSubscribedChannels(), ["test"])
-    t.deepEqual(notifications, [
+    expect(hub.getSubscribedChannels()).to.be.eql(["test"])
+    expect(notifications).to.be.eql([
       {
         channel: "test",
         payload: { hello: "world" },
         processId: notifications[0] ? notifications[0].processId : 0
       }
     ])
-    t.deepEqual(receivedPayloads, [
+    expect(receivedPayloads).to.be.eql([
       { hello: "world" }
     ])
-    t.is(reconnects, 1)
-    t.is(connectedEvents, 2)
+    expect(reconnects).to.be.eql(1)
+    expect(connectedEvents).to.be.eql(2)
   } finally {
     debug("Closing the subscriber")
     await hub.close()
